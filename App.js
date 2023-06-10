f060ae1e19055b4e5d561ff849d39dba52aee33e
euemmm <jungju.lee@paygate.net>
2023-06-11 01:37:02 +0900

    init

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './components/LoginScreen';
+import HomeScreen from './components/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+	
+    return (
+
+		<SafeAreaView style = {{width: '100%', height: '100%'}}>
+
+			<NavigationContainer style = {{width: '100%', height: '100%'}}>
+
+				<Stack.Navigator style = {{width: '100%', height: '100%'}}>
+
+					<Stack.Screen
+						style = {{width: '100%', height: '100%'}}
+						name = "Login"
+						component = {LoginScreen}
+					/>
+
+					<Stack.Screen
+						style = {{width: '100%', height: '100%'}}
+						name = "Home"
+						component = {HomeScreen}
+					/>
+					
+				</Stack.Navigator>
+
+			</NavigationContainer>
+
+		</SafeAreaView>
+
+    )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import {View, Pressable, Text, TextInput, StyleSheet} from 'react-native';
+
+const Home = ({navigator}) => {
+
+    return(
+
+        <View style = {{flex: 1}}>
+
+            <Text style = {{width: '100%'}}> I will be adding AI related material here. </Text>
+            <Text style = {{width: '100%'}}> I have created my own AI API. it's related with MNIST dataset. </Text>
+
+        </View>
+
+    )
+
+}
+
+export default Home
